7c0008a138a7e834c9cef4fe18ab9682d1ebf074
william <espiw@farmingdale.edu>
2025-09-22 23:57:56 -0400

    Bro Code nested classes

---
+++ nested_class.py
@@ -0,0 +1,45 @@
+# nested class: a class defined within another class
+#           class Outer:
+#               class Inner:
+
+# benefits: allows you to logically group classes that are closely related
+#           encapsulates private details that arent relevant outside of the outer class
+#           keeps the namespace clean; reduces the posibility of naming conflicts
+
+class Company:
+    class Employee:
+        def __init__(self, name, position):
+            self.name = name
+            self.position = position
+
+        def get_details(self):
+            return f"{self.name} {self.position}"
+
+    def __init__(self, company_name):
+        self.company_name = company_name
+        self.employees = []
+
+    def add_employee(self, name, position):
+        new_employee = self.Employee(name, position)
+        self.employees.append(new_employee)
+
+    def list_employees(self):
+        return [employee.get_details() for employee in self.employees]
+
+company1 = Company("Krusty Krab")
+company2 = Company("Chum Bucket")
+
+company1.add_employee("Eugene", "Manager")
+company1.add_employee("Spongebob", "Frycook")
+company1.add_employee("Squidward", "Cashier")
+
+company2.add_employee("Sheldon", "Manager")
+company2.add_employee("Karen", "Assistant")
+
+print(company1.list_employees())
+for employee in company1.list_employees():
+    print(employee)
+
+print(company2.list_employees())
+for employee in company2.list_employees():
+    print(employee)
